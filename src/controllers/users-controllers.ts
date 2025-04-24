61220828f64308b6c4d5448da79c61291f87786d
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-04-24 13:11:56 -0300

    Criptografando Senha do Usuário

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
+import { hash } from "bcrypt";
 import { z } from "zod";
 
 class UserController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
     const bodySchema = z.object({
       name: z.string().trim().min(3),
       email: z.string().email(),
@@ -11,7 +12,9 @@ class UserController {
 
     const { name, email, password } = bodySchema.parse(request.body);
 
-    return response.json({ message: "ok" });
+    const hashedPassword = await hash(password, 8);
+
+    return response.json({ message: "ok", hashedPassword });
   }
 };
 
