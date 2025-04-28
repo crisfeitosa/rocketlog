0ec2d395299b6071e400237a5e3bcd6e008bd9ba
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-04-28 14:37:31 -0300

    Cadastrando Entrega

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,24 @@
+import { prisma } from "@/database/prisma";
 import { Request, Response } from "express";
+import { z } from "zod";
 
 class DeliveriesController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description
+      }
+    });
+
+    return response.status(201).json();
   }
 }
 
